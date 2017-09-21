1b4b0ec8ff8ab8dc5b1130f868379dc5dc216404
khamvin <khamvin@outlook.com>
2017-09-21 20:33:08 +0700

    fristcode in php

---
+++ ex-1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ຕົວຢ່າງ1(ex-1)</title>
+        <style>
+            body{
+                font-family: Phetsarath OT;
+                background-color: beige
+            }
+        </style>
+    </head>
+    <body>
+        ສະບາຍດີ
+        <?php
+      //
+        echo "Welcome to Php<br> ";
+        print("Have a good trip")
+      
+        
+        ?>
+    </body>
+</html>
