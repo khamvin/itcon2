b5e4520eba0378b1a933435088d6ff6aa4acdb09
khamvin <khamvin@outlook.com>
2017-09-26 19:58:24 +0700

    ex2-fristcode in php

---
+++ ex2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+         $x =5 /*ບວກກັບ 5 */ + 5;
+       echo "x = $x <br>";
+       echo 'x = $x <br>';
+       $color = "ສີແດງ";
+       echo"ລົດຂອງຂ້ອຍສີ:$color <br>";
+       echo"ເຮືອນຂອງຂ້ອຍສີ:$COLOR <br>";
+       
+        ?>
+    </body>
+</html>
