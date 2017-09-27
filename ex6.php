cfaaa0e3a8768b6606422257c027363bd847738a
khamvin <khamvin@outlook.com>
2017-09-27 19:05:10 +0700

    ໂປຮແກມຄິດໄລ່ເລກIT_Con2-fristcode in php

--- ex6.php
+++ ex6.php
@@ -8,11 +8,28 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <style>
+            body{
+                font-family: Phetsarath OT;
+                background-color: beige
+            }
+        </style>
+
     </head>
     <body>
-        <?php
-        // put your c   static $x = 1;
-        
-        ?>
+        <h3>ໂປຮແກມຄິດໄລ່ເລກ</h3>
+        <form action="" method="post">
+            <input type="text" name="num1" value=""  required="">
+            <input type="radio" name="operator" value="+">+
+            <input type="radio" name="operator" value="-">-
+            <input type="radio" name="operator" value="*">*
+            <input type="radio" name="operator" value="/">/
+            <input type="text" name="num2" value="" required="">
+            <input type="submit" name="equal" value="=">
+            <button onclick="window.location.reload(true)">Refresh</button>
+        </form>
+
+        <br><hr>
     </body>
+
 </html>
